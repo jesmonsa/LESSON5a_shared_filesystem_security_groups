0195d01c69bdc4054c5597ecd846ea3d47565282
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-19 06:00:28 -0500

    Tu mensaje de commit

---
+++ LESSON5a_shared_filesystem_security_groups/compartment.tf
@@ -0,0 +1,10 @@
+resource "oci_identity_compartment" "Prod_01" {
+  provider = oci.homeregion
+  name = "Compartment"
+  description = "Compartment Prod_01"
+  compartment_id = var.compartment_ocid
+  
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
