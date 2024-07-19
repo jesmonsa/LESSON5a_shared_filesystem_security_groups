d45855bc9e6151bbb8353dac18a8d89abfc8e8d7
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-19 06:06:32 -0500

    Ultimo commit

--- LESSON5a_shared_filesystem_security_groups/compartment.tf
+++ LESSON5a_shared_filesystem_security_groups/compartment.tf
@@ -1,6 +1,6 @@
 resource "oci_identity_compartment" "Prod_01" {
   provider = oci.homeregion
-  name = "Compartment"
+  name = "CompartmentProd_01"
   description = "Compartment Prod_01"
   compartment_id = var.compartment_ocid
   
