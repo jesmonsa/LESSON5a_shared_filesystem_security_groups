aae88255e14e4e75df8f37f0cd751672dc315841
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-19 06:09:18 -0500

    Ultimo commit

--- LESSON5a_shared_filesystem_security_groups/datasources.tf
+++ LESSON5a_shared_filesystem_security_groups/datasources.tf
@@ -28,37 +28,37 @@ data "oci_core_images" "OSImage" {
 }
 
 # Bastion Compute VNIC Attachment DataSource
-data "oci_core_vnic_attachments" "FoggyKitchenBastionServer_VNIC1_attach" {
+data "oci_core_vnic_attachments" "BastionServer_VNIC1_attach" {
   availability_domain = var.availablity_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availablity_domain_name
-  compartment_id      = oci_identity_compartment.FoggyKitchenCompartment.id
-  instance_id         = oci_core_instance.FoggyKitchenBastionServer.id
+  compartment_id      = oci_identity_compartment.Prod_01.id
+  instance_id         = oci_core_instance.BastionServer.id
 }
 
 # Bastion Compute VNIC DataSource
-data "oci_core_vnic" "FoggyKitchenBastionServer_VNIC1" {
-  vnic_id = data.oci_core_vnic_attachments.FoggyKitchenBastionServer_VNIC1_attach.vnic_attachments.0.vnic_id
+data "oci_core_vnic" "BastionServer_VNIC1" {
+  vnic_id = data.oci_core_vnic_attachments.BastionServer_VNIC1_attach.vnic_attachments.0.vnic_id
 }
 
 # WebServer1 Compute VNIC Attachment DataSource
-data "oci_core_vnic_attachments" "FoggyKitchenWebserver1_VNIC1_attach" {
+data "oci_core_vnic_attachments" "Webserver1_VNIC1_attach" {
   availability_domain = var.availablity_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availablity_domain_name
-  compartment_id      = oci_identity_compartment.FoggyKitchenCompartment.id
-  instance_id         = oci_core_instance.FoggyKitchenWebserver1.id
+  compartment_id      = oci_identity_compartment.Prod_01.id
+  instance_id         = oci_core_instance.Webserver1.id
 }
 
 # WebServer1 Compute VNIC DataSource
 data "oci_core_vnic" "FoggyKitchenWebserver1_VNIC1" {
-  vnic_id = data.oci_core_vnic_attachments.FoggyKitchenWebserver1_VNIC1_attach.vnic_attachments.0.vnic_id
+  vnic_id = data.oci_core_vnic_attachments.Webserver1_VNIC1_attach.vnic_attachments.0.vnic_id
 }
 
 # WebServer2 Compute VNIC Attachment DataSource
 data "oci_core_vnic_attachments" "FoggyKitchenWebserver2_VNIC1_attach" {
   availability_domain = var.availablity_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availablity_domain_name
-  compartment_id      = oci_identity_compartment.FoggyKitchenCompartment.id
-  instance_id         = oci_core_instance.FoggyKitchenWebserver2.id
+  compartment_id      = oci_identity_compartment.Prod_01.id
+  instance_id         = oci_core_instance.Webserver2.id
 }
 
 # WebServer2 Compute VNIC DataSource
 data "oci_core_vnic" "FoggyKitchenWebserver2_VNIC1" {
-  vnic_id = data.oci_core_vnic_attachments.FoggyKitchenWebserver2_VNIC1_attach.vnic_attachments.0.vnic_id
+  vnic_id = data.oci_core_vnic_attachments.Webserver2_VNIC1_attach.vnic_attachments.0.vnic_id
 }
