5d617505a3a65e0441927563c4fd671122ec5dd5
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-19 06:27:57 -0500

    Ultimo commit

--- LESSON5a_shared_filesystem_security_groups/remote.tf
+++ LESSON5a_shared_filesystem_security_groups/remote.tf
@@ -1,29 +1,29 @@
 # Setup FSS on Webserver1
 
-resource "null_resource" "FoggyKitchenWebserver1SharedFilesystem" {
-  depends_on = [oci_core_instance.FoggyKitchenWebserver1, oci_core_instance.FoggyKitchenBastionServer, oci_file_storage_export.FoggyKitchenExport]
+resource "null_resource" "Webserver1SharedFilesystem" {
+  depends_on = [oci_core_instance.Webserver1, oci_core_instance.BastionServer, oci_file_storage_export.Export]
 
   provisioner "remote-exec" {
     connection {
       type                = "ssh"
       user                = "opc"
-      host                = data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address
+      host                = data.oci_core_vnic.Webserver1_VNIC1.private_ip_address
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
       timeout             = "10m"
-      bastion_host        = data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address
+      bastion_host        = data.oci_core_vnic.BastionServer_VNIC1.public_ip_address
       bastion_port        = "22"
       bastion_user        = "opc"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
     }
     inline = [
-      "echo '== Start of null_resource.FoggyKitchenWebserver1SharedFilesystem'",
+      "echo '== Start of null_resource.Webserver1SharedFilesystem'",
       "sudo /bin/su -c \"yum install -y -q nfs-utils\"",
       "sudo /bin/su -c \"mkdir -p /sharedfs\"",
       "sudo /bin/su -c \"echo '${var.MountTargetIPAddress}:/sharedfs /sharedfs nfs rsize=8192,wsize=8192,timeo=14,intr 0 0' >> /etc/fstab\"",
       "sudo /bin/su -c \"mount /sharedfs\"",
-      "echo '== End of null_resource.FoggyKitchenWebserver2SharedFilesystem'"
+      "echo '== End of null_resource.Webserver2SharedFilesystem'"
     ]
   }
 
@@ -31,30 +31,30 @@ resource "null_resource" "FoggyKitchenWebserver1SharedFilesystem" {
 
 # Setup FSS on Webserver2
 
-resource "null_resource" "FoggyKitchenWebserver2SharedFilesystem" {
-  depends_on = [oci_core_instance.FoggyKitchenWebserver2, oci_core_instance.FoggyKitchenBastionServer, oci_file_storage_export.FoggyKitchenExport]
+resource "null_resource" "Webserver2SharedFilesystem" {
+  depends_on = [oci_core_instance.Webserver2, oci_core_instance.BastionServer, oci_file_storage_export.Export]
 
   provisioner "remote-exec" {
     connection {
       type                = "ssh"
       user                = "opc"
-      host                = data.oci_core_vnic.FoggyKitchenWebserver2_VNIC1.private_ip_address
+      host                = data.oci_core_vnic.Webserver2_VNIC1.private_ip_address
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
       timeout             = "10m"
-      bastion_host        = data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address
+      bastion_host        = data.oci_core_vnic.BastionServer_VNIC1.public_ip_address
       bastion_port        = "22"
       bastion_user        = "opc"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
     }
     inline = [
-      "echo '== Start of null_resource.FoggyKitchenWebserver2SharedFilesystem'",
+      "echo '== Start of null_resource.Webserver2SharedFilesystem'",
       "sudo /bin/su -c \"yum install -y -q nfs-utils\"",
       "sudo /bin/su -c \"mkdir -p /sharedfs\"",
       "sudo /bin/su -c \"echo '${var.MountTargetIPAddress}:/sharedfs /sharedfs nfs rsize=8192,wsize=8192,timeo=14,intr 0 0' >> /etc/fstab\"",
       "sudo /bin/su -c \"mount /sharedfs\"",
-      "echo '== Start of null_resource.FoggyKitchenWebserver2SharedFilesystem'"
+      "echo '== Start of null_resource.Webserver2SharedFilesystem'"
     ]
   }
 
@@ -62,18 +62,18 @@ resource "null_resource" "FoggyKitchenWebserver2SharedFilesystem" {
 
 # Software installation within WebServer1 Instance
 
-resource "null_resource" "FoggyKitchenWebserver1HTTPD" {
-  depends_on = [oci_core_instance.FoggyKitchenWebserver1, oci_core_instance.FoggyKitchenBastionServer, null_resource.FoggyKitchenWebserver1SharedFilesystem]
+resource "null_resource" "Webserver1HTTPD" {
+  depends_on = [oci_core_instance.Webserver1, oci_core_instance.BastionServer, null_resource.Webserver1SharedFilesystem]
   provisioner "remote-exec" {
     connection {
       type                = "ssh"
       user                = "opc"
-      host                = data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address
+      host                = data.oci_core_vnic.Webserver1_VNIC1.private_ip_address
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
       timeout             = "10m"
-      bastion_host        = data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address
+      bastion_host        = data.oci_core_vnic.BastionServer_VNIC1.public_ip_address
       bastion_port        = "22"
       bastion_user        = "opc"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
@@ -83,7 +83,7 @@ resource "null_resource" "FoggyKitchenWebserver1HTTPD" {
 
       "echo '== 2. Creating /sharedfs/index.html'",
       "sudo -u root touch /sharedfs/index.html",
-      "sudo /bin/su -c \"echo 'Welcome to FoggyKitchen.com! These are both WEBSERVERS under LB umbrella with shared index.html ...' > /sharedfs/index.html\"",
+      "sudo /bin/su -c \"echo 'Welcome to Example.com! These are both WEBSERVERS under LB umbrella with shared index.html ...' > /sharedfs/index.html\"",
 
       "echo '== 3. Adding Alias and Directory sharedfs to /etc/httpd/conf/httpd.conf'",
       "sudo /bin/su -c \"echo 'Alias /shared/ /sharedfs/' >> /etc/httpd/conf/httpd.conf\"",
@@ -103,18 +103,18 @@ resource "null_resource" "FoggyKitchenWebserver1HTTPD" {
 
 # Software installation within WebServer2 Instance
 
-resource "null_resource" "FoggyKitchenWebserver2HTTPD" {
-  depends_on = [null_resource.FoggyKitchenWebserver1HTTPD, oci_core_instance.FoggyKitchenWebserver2, oci_core_instance.FoggyKitchenBastionServer, null_resource.FoggyKitchenWebserver2SharedFilesystem]
+resource "null_resource" "Webserver2HTTPD" {
+  depends_on = [null_resource.Webserver1HTTPD, oci_core_instance.Webserver2, oci_core_instance.BastionServer, null_resource.Webserver2SharedFilesystem]
   provisioner "remote-exec" {
     connection {
       type                = "ssh"
       user                = "opc"
-      host                = data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address
+      host                = data.oci_core_vnic.Webserver1_VNIC1.private_ip_address
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
       timeout             = "10m"
-      bastion_host        = data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address
+      bastion_host        = data.oci_core_vnic.BastionServer_VNIC1.public_ip_address
       bastion_port        = "22"
       bastion_user        = "opc"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
